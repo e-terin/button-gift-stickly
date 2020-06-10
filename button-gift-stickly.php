d6e45fd6603a7eaa96aeee6c95ef98457bb371aa
Evgeniy Terin <web@derin.ru>
2020-06-10 13:52:22 +0300

    Add main file

---
+++ button-gift-stickly.php
@@ -0,0 +1,8 @@
+<?php
+/*
+Plugin Name: Button Gift Stickly
+Description: Integrates a floating contact button and opens an modal with your favorite contact form.
+Version: 1.0.0
+Author: Evgeniy Terin
+Author URI: https://web.derin.ru
+*/
