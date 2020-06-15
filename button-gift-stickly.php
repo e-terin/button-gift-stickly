bfc22fff211239d600b0212e0d0efa4e47c5f7b5
Evgeniy Terin <web@derin.ru>
2020-06-15 16:23:19 +0300

    Structure from Floating contact plugin

--- button-gift-stickly.php
+++ button-gift-stickly.php
@@ -7,17 +7,9 @@ Author: Evgeniy Terin
 Author URI: https://web.derin.ru
 */
 
-/*==Plugin internal settings==*/
-$img_gift_filename='gift.png';      /*==Name with extensions of gift image file==*/
-$style_filename='bgs';              /*==Name without extensions of main CSS style file*/
-$use_min_style=false;               /*==Setting set to use minificated version of main CSS style file ('true') or not use ('false')*/
-
-/*==Set style filename==*/
-$style=($use_min_style)?$style_filename.'.min.css':$style_filename.'.css';
-
 /*==Add plugin styles==*/
 function bgs_styles() {
-    wp_enqueue_style( 'custom', plugins_url('assets/css/bgs.css',__FILE__), array(), 1.0, 'screen' );
+    wp_enqueue_style( 'bgs', plugins_url('/assets/css/bgs.css',__FILE__), array(), 1.0, 'screen' );
 }
 add_action( 'wp_enqueue_scripts', 'bgs_styles');
 
@@ -27,17 +19,17 @@ function bgs_footer() {
     ?>
     <a href="#bgs-modal" class="bgs-link-button">
         <div>
-            <div>5 000 р</div>
-            <img src="<?=plugins_url('assets/img/'.$img_gift,__FILE__)?>">
+            <div>5 000₽</div>
+            <img src="<?=plugins_url('/assets/img/gift.png',__FILE__)?>">
         </div>
     </a>
-  <div id="bgs-modal"> 
-  <div class="bgs-footer">
-    <a href="#" class="close-bgs-modal"><div class="bgs-footer-close"><span class="bgs-close">X</span></div></a></div>
-  </div>
-  <div class="bgs-modal-content"> 
-    123
-    <div class="overlay"></div> 
-  </div>
+  <div id="bgs-modal">
+    <div class="bgs-header">
+      <a href="#" class="close-bgs-modal"><div class="bgs-footer-close"><span class="bgs-close">X</span></div></a></div>
+    </div>
+    <div class="bgs-modal-content"> 
+      123
+      <div class="bgs-overlay"></div>
+    </div>
   <?php
 }
